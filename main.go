207ab2b7c84f0fb1dad0550d29a926c4e11f26a0
Yudi Wiradinata <yudi.wiradinata@tiket.com>
2019-08-23 16:46:59 +0700

    struct

--- main.go
+++ main.go
@@ -1,44 +1,86 @@
 package main
 
+import "fmt"
+
 func main() {
-	//var card string = "Joker"
-	//card := "Joker"
-	//card = "Five"
-	//card := newCard()
-	//fmt.Println(card)
-	//cards := []string{"Joker", newCard()}
-	//cards := deck{"Joker", newCard()}
-	//cards = append(cards, "six of spades")
-
-	//cards := newDeck()
-
-	cards := newDeckFromFile("tes_cards")
-	cards.shuffle()
-
-	hand, remainingCard := deal(cards, 5)
-
-	hand.print()
-	remainingCard.print()
-
-	//cards.print()
-
-	//save to file
-	//cards.toFile("tes_cards")
-
-	//cards.print()
 	/*
-		for i, card := range cards {
-			fmt.Println(i, card)
-		}
+		//cara pertama buat struct
+		alex := person{firstName: "Alex", lastName: "Anderson"}
 	*/
+
+	//cara kedua
+	/*contact := contactInfo{email: "tes@ff.com", zip: 12345}
+
+	var alex person
+	alex.firstName = "Ales"
+	alex.lastName = "Teles"
+	alex.contact = contact
+	*/
+
+	alex := person{
+		firstName: "Alex",
+		lastName:  "Teles",
+		contact: contactInfo{
+			email: "Afds@tes.com",
+			zip:   12345,
+		},
+	}
+
+	rere := person{
+		firstName: "RERE",
+		lastName:  "HEGE",
+		contact: contactInfo{
+			email: "de@dslf.com",
+			zip:   432145,
+		},
+	}
+
+	//alexPointer := &alex
+
+	//alex.updatePerson(rere)
+	//alex.updateName("LEXA")
+	//alex.updateNameNoPointer("TES")
+
+	updatePerson(alex)
+
+	alex.print()
+
+	//Map
+
+	personMap := make(map[string]person)
+
+	personMap["alex"] = alex
+	personMap["rere"] = rere
+
+	// colors := map[string]string{
+	// 	"red":  "00001",
+	// 	"blue": "00002",
+	// }
+
+	//delete(personMap, "rere")
+
+	//printMap(personMap)
+
+	//fmt.Println(personMap["rere"].contact.zip)
 }
 
-func newCard() string {
-	return "Six Secop"
+func printMap(c map[string]person) {
+	for name, person := range c {
+		fmt.Println("name of person", name, "is", person.firstName)
+	}
+
+}
+func updatePerson(per person) {
+	per.firstName = "TES"
 }
 
-// array in go
-// array
-// Fixed length list of things
-// slice
-// an array that can grow or shrink
+func (p *person) updateName(new string) {
+	(*p).firstName = new
+}
+
+func (p person) updateNameNoPointer(new string) {
+	p.firstName = new
+}
+func (p person) print() {
+	fmt.Printf("%v", p)
+}
