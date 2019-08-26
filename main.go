c103a2917b9ee6aa9433c66f243e3c26b01a310c
Yudi Wiradinata <yudi.wiradinata@tiket.com>
2019-08-26 17:13:56 +0700

    init interfaces

--- main.go
+++ main.go
@@ -2,85 +2,30 @@ package main
 
 import "fmt"
 
+type bd interface {
+	getGreeting() string
+}
+
+type englishBot struct{}
+type spanishBot struct{}
+
 func main() {
-	/*
-		//cara pertama buat struct
-		alex := person{firstName: "Alex", lastName: "Anderson"}
-	*/
+	eb := englishBot{}
+	sb := spanishBot{}
 
-	//cara kedua
-	/*contact := contactInfo{email: "tes@ff.com", zip: 12345}
-
-	var alex person
-	alex.firstName = "Ales"
-	alex.lastName = "Teles"
-	alex.contact = contact
-	*/
-
-	alex := person{
-		firstName: "Alex",
-		lastName:  "Teles",
-		contact: contactInfo{
-			email: "Afds@tes.com",
-			zip:   12345,
-		},
-	}
-
-	rere := person{
-		firstName: "RERE",
-		lastName:  "HEGE",
-		contact: contactInfo{
-			email: "de@dslf.com",
-			zip:   432145,
-		},
-	}
-
-	//alexPointer := &alex
-
-	//alex.updatePerson(rere)
-	//alex.updateName("LEXA")
-	//alex.updateNameNoPointer("TES")
-
-	updatePerson(alex)
-
-	alex.print()
-
-	//Map
-
-	personMap := make(map[string]person)
-
-	personMap["alex"] = alex
-	personMap["rere"] = rere
-
-	// colors := map[string]string{
-	// 	"red":  "00001",
-	// 	"blue": "00002",
-	// }
-
-	//delete(personMap, "rere")
-
-	//printMap(personMap)
-
-	//fmt.Println(personMap["rere"].contact.zip)
-}
-
-func printMap(c map[string]person) {
-	for name, person := range c {
-		fmt.Println("name of person", name, "is", person.firstName)
-	}
+	printGreeting(eb)
+	printGreeting(sb)
 
 }
-func updatePerson(per person) {
-	per.firstName = "TES"
+
+func printGreeting(o bd) {
+	fmt.Println(o.getGreeting())
 }
 
-func (p *person) updateName(new string) {
-	(*p).firstName = new
+func (eb englishBot) getGreeting() string {
+	return "Hi there"
 }
 
-func (p person) updateNameNoPointer(new string) {
-	p.firstName = new
-}
-func (p person) print() {
-	fmt.Printf("%v", p)
+func (sb spanishBot) getGreeting() string {
+	return "hola"
 }
